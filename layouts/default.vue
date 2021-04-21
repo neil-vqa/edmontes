a8d534da1623d0f44ca9179869c622445b3487af
Neil Van <nvq.alino@gmail.com>
2021-04-21 16:20:49 +0800

    First commit

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+html {
+  font-family: 'Quicksand', sans-serif;
+}
+
+body {
+  /* background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' version='1.1' xmlns:xlink='http://www.w3.org/1999/xlink' xmlns:svgjs='http://svgjs.com/svgjs' width='1920' height='1080' preserveAspectRatio='none' viewBox='0 0 1920 1080'%3e%3cg mask='url(%26quot%3b%23SvgjsMask1043%26quot%3b)' fill='none'%3e%3cpath d='M-43.33 770.09C211.83 764.36 427.14 412.28 913.31 413.69 1399.49 415.1 1575.22 1081.53 1869.96 1118.16' stroke='rgba(237%2c 242%2c 247%2c 1)' stroke-width='2'%3e%3c/path%3e%3cpath d='M-198.3 127.54C8.06 141.67 84.24 489.57 520.09 537.94 955.94 586.31 1008.11 1096.74 1238.48 1133.79' stroke='rgba(237%2c 242%2c 247%2c 1)' stroke-width='2'%3e%3c/path%3e%3cpath d='M-112.4 978.17C109.09 955.8 248.86 479.64 626.61 481.37 1004.36 483.1 1101.39 1218.69 1365.62 1292.24' stroke='rgba(237%2c 242%2c 247%2c 1)' stroke-width='2'%3e%3c/path%3e%3cpath d='M-68.75 161.34C147.28 169.5 255.98 478.12 719.11 517.74 1182.24 557.36 1257.45 1111.09 1506.97 1148.29' stroke='rgba(237%2c 242%2c 247%2c 1)' stroke-width='2'%3e%3c/path%3e%3cpath d='M-217.59 498.33C26.46 495.98 220.1 227.09 723.61 239.13 1227.13 251.17 1335.88 1133.54 1664.82 1216.22' stroke='rgba(237%2c 242%2c 247%2c 1)' stroke-width='2'%3e%3c/path%3e%3c/g%3e%3cdefs%3e%3cmask id='SvgjsMask1043'%3e%3crect width='1920' height='1080' fill='white'%3e%3c/rect%3e%3c/mask%3e%3c/defs%3e%3c/svg%3e"); */
+  background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' version='1.1' xmlns:xlink='http://www.w3.org/1999/xlink' xmlns:svgjs='http://svgjs.com/svgjs' width='1920' height='1080' preserveAspectRatio='none' viewBox='0 0 1920 1080'%3e%3cg mask='url(%26quot%3b%23SvgjsMask1067%26quot%3b)' fill='none'%3e%3cpath d='M-342.23 390.43C-137.89 413.65 24.88 832.83 329.25 865.63 633.62 898.43 564.94 1077.19 664.99 1086.83' stroke='rgba(237%2c 242%2c 247%2c 1)' stroke-width='2'%3e%3c/path%3e%3cpath d='M-28.28 448.41C222.3 461.11 380.61 886.29 866.99 902.01 1353.36 917.73 1525.44 1207.57 1762.25 1211.91' stroke='rgba(237%2c 242%2c 247%2c 1)' stroke-width='2'%3e%3c/path%3e%3cpath d='M-116.38 220.81C63.43 267.59 53.54 692.55 394.24 760.81 734.93 829.07 743.93 1140.34 904.85 1163.59' stroke='rgba(237%2c 242%2c 247%2c 1)' stroke-width='2'%3e%3c/path%3e%3cpath d='M-155.64 390.72C31.36 403.01 180.75 704.23 497.86 757.92 814.97 811.61 694.04 1167.39 824.61 1223.91' stroke='rgba(237%2c 242%2c 247%2c 1)' stroke-width='2'%3e%3c/path%3e%3cpath d='M-160.76 377.15C72.29 392.04 277.88 783.29 655.83 830.75 1033.79 878.21 917.51 1241.82 1064.13 1283.4' stroke='rgba(237%2c 242%2c 247%2c 1)' stroke-width='2'%3e%3c/path%3e%3c/g%3e%3cdefs%3e%3cmask id='SvgjsMask1067'%3e%3crect width='1920' height='1080' fill='white'%3e%3c/rect%3e%3c/mask%3e%3c/defs%3e%3c/svg%3e");
+}
+</style>
